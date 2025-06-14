7de84f2d8f5705acf3d1dc959b9f1cf862040b73
YukikoNii <NIIY64308@gmail.com>
2025-06-14 08:47:13 +0100

    feat: display monthly calendar, with prev/next button to view prev/next month

--- src/Personal-Project-Calendar.js
+++ src/Personal-Project-Calendar.js
@@ -1,167 +1,65 @@
-// get today's date
-const date = new Date();
-
-// months array
-const months = [
-  'January',
-  'February',
-  'March',
-  'April',
-  'May',
-  'June',
-  'July',
-  'August',
-  'September',
-  'October',
-  'November',
-  'December'
-];
-
-function renderCalendar() {
-  // set the date to 1
-  date.setDate(1);
-
-
-  const monthDays = document.querySelector('.days');
-
-  const lastDate = new Date(date.getFullYear(), date.getMonth() + 1, 0)
-
-  // get last day of the month
-  const lastDay = lastDate.getDate();
-
-  // get day of the week of the first day of the month
-  const firstDayIndex = date.getDay();
-
-  // get last day of last month
-  const prevLastDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
-
-
-  // get day of the week of the last day of the month
-  const lastDayIndex = lastDate.getDay();
-
-  // calculate how many days of the next month will be on the calendar
-  const nextMonthDays = 6 - lastDayIndex;
-
-  // show month and year as header
-  document.querySelector('.date h1').innerHTML = months[date.getMonth()] + ' ' + date.getFullYear();
-  // show today's date
-  document.querySelector('.date p').innerHTML = new Date().toDateString();
-
-  let days = '';
-
-  // display last few days of the last month, depending on what day of the week this month starts
-  for (let i = firstDayIndex; i > 0; i--) {
-    days += `<div class="prevDate">${prevLastDay - i + 1}</div>`;
-  }
-
-  for (let i = 1; i <= lastDay; i++) {
-    // if the date matches with today's date
-    if (i === new Date().getDate() && date.getMonth() === new Date().getMonth() && date.getFullYear() === new Date().getFullYear()) {
-      days += `<div class="today"><span>${i}</span></div>`;
-    } else {
-      days += `<div class="day"><span>${i}</span></div>`;
-    }
-  }
-
-
-  // display the first few days of next month
-  for (let i = 1; i <= nextMonthDays; i++) {
-    days += `<div class="nextDate">${i}</div>`;
-  }
-
-  monthDays.innerHTML = days;
-}
-
-
-// get prev arrow
-let prev = document.querySelector('.prev');
-prev.addEventListener('click', prevMonth);
-prev.addEventListener('click', renderEvents);
-// get next arrow
-let next = document.querySelector('.next');
-next.addEventListener('click', nextMonth);
-next.addEventListener('click', renderEvents);
-
-function nextMonth() {
-  // increase the month by 1
-  date.setMonth(date.getMonth() + 1);
-  renderCalendar();
-}
-
-function prevMonth() {
-  // decrease the month by 1
-  date.setMonth(date.getMonth() - 1);
-  renderCalendar();
-}
-
-renderCalendar();
-
-
 /*
 
 events
 
 */
 
-
-window.addEventListener('load', renderEvents);
+window.addEventListener("load", renderEvents);
 
 function renderEvents() {
   // get date divs
-  let day = document.getElementsByClassName('day');
+  let day = document.getElementsByClassName("day");
   // get the span that contains the date
-  let dayVal = document.querySelectorAll('.day > span');
+  let dayVal = document.querySelectorAll(".day > span");
   // create an array of dates in two digits
   let dayValArr = [];
   for (var i = 0; i < dayVal.length; i++) {
     let num = dayVal[i].innerHTML;
     if (num < 10) {
-      dayValArr.push('0' + num);
+      dayValArr.push("0" + num);
     } else {
       dayValArr.push(num);
     }
   }
 
-  let headDate = document.querySelector('.date > h1').innerHTML.split(' ');
+  let headDate = document.querySelector(".date > h1").innerHTML.split(" ");
   // get month from the display
   let month = headDate[0];
   // get year from the display
   let year = headDate[1];
 
-  let correctMonth = '';
+  let correctMonth = "";
   for (var i = 0; i < months.length; i++) {
     // months is the array that contains the names of months
     if (months[i] === month) {
       if (i < 9) {
         let monthNum = i + 1;
-        correctMonth = '0' + monthNum;
+        correctMonth = "0" + monthNum;
       } else {
         correctMonth = i + 1;
       }
     }
   }
 
-
   let dateArr = [];
-
 
   for (var i = 0; i < dayValArr.length; i++) {
     // format: yyyy-mm-dd
-    dateArr.push(year + '-' + correctMonth + '-' + dayValArr[i]);
+    dateArr.push(year + "-" + correctMonth + "-" + dayValArr[i]);
   }
 
   // local storage, saved in todo.js
-  let getDateArr = JSON.parse(window.localStorage.getItem('getDateArr'));
+  let getDateArr = JSON.parse(window.localStorage.getItem("getDateArr"));
 
   // name of the tasks
-  let entryIdArr = JSON.parse(window.localStorage.getItem('entryIdArr'));
+  let entryIdArr = JSON.parse(window.localStorage.getItem("entryIdArr"));
   if (getDateArr !== null) {
     for (let i = 0; i < getDateArr.length; i++) {
       for (let j = 0; j < dateArr.length; j++) {
         if (getDateArr[i] === dateArr[j]) {
           // create div when they have the same date
-          let eventCal = document.createElement('div');
-          eventCal.className = 'eventCal';
+          let eventCal = document.createElement("div");
+          eventCal.className = "eventCal";
           // display the title of the task
           eventCal.innerHTML = entryIdArr[i];
           day[j].appendChild(eventCal);
@@ -172,17 +70,17 @@ function renderEvents() {
 
   // get today's date in ISO format
   let todayDate = new Date().toISOString().slice(0, 10);
-  let today = document.querySelector('.today');
+  let today = document.querySelector(".today");
   if (today !== null && getDateArr !== null) {
     for (let i = 0; i < getDateArr.length; i++) {
       // if date is today
       if (getDateArr[i] === todayDate) {
-        let eventCal = document.createElement('div');
-        eventCal.className = 'eventCal';
+        let eventCal = document.createElement("div");
+        eventCal.className = "eventCal";
         // display the title of the task
         eventCal.innerHTML = entryIdArr[i];
         today.appendChild(eventCal);
       }
     }
   }
-};
+}
--- src/pages/CalendarPage.tsx
+++ src/pages/CalendarPage.tsx
@@ -4,10 +4,128 @@ import Sidebar from "../components/Sidebar";
 import { Link } from "react-router-dom";
 import AppHeader from "../components/AppHeader";
 import "material-icons/iconfont/material-icons.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function Calendar() {
   const [isOpen, setIsOpen] = useState(true);
+  const [days, setDays] = useState<JSX.Element[]>([]); // create an array of JSX Elements
+  const [date, setDate] = useState(new Date());
+
+  // months array
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  useEffect(() => {
+    // set the date to 1
+    date.setDate(1);
+
+    const lastDate = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+
+    // get last day of the month
+    const lastDay = lastDate.getDate();
+
+    // get day of the week of the first day of the month
+    const firstDayIndex = date.getDay();
+
+    // get last day of last month
+    const prevLastDay = new Date(
+      date.getFullYear(),
+      date.getMonth(),
+      0
+    ).getDate();
+
+    // get day of the week of the last day of the month
+    const lastDayIndex = lastDate.getDay();
+
+    // calculate how many days of the next month will be on the calendar
+    const nextMonthDays = 6 - lastDayIndex;
+
+    let newDays: JSX.Element[] = [];
+
+    // display last few days of the last month, depending on what day of the week this month starts
+    for (let i = firstDayIndex; i > 0; i--) {
+      newDays.push(
+        <div
+          key={String(date.getMonth() - 1) + String(i)}
+          className={styles.prevDate}
+        >
+          {prevLastDay - i + 1}
+        </div>
+      ); // Push JSX elements
+    }
+
+    for (let i = 1; i <= lastDay; i++) {
+      // if the date matches with today's date
+      if (
+        i === new Date().getDate() &&
+        date.getMonth() === new Date().getMonth() &&
+        date.getFullYear() === new Date().getFullYear()
+      ) {
+        newDays.push(
+          <div
+            key={String(date.getMonth()) + String(i)}
+            className={styles.today}
+          >
+            <span>{i}</span>
+          </div>
+        );
+      } else {
+        newDays.push(
+          <div
+            key={String(date.getMonth() + 1) + String(i)}
+            className={styles.day}
+          >
+            <span>{i}</span>
+          </div>
+        );
+      }
+    }
+
+    // display the first few days of next month
+    for (let i = 1; i <= nextMonthDays; i++) {
+      newDays.push(
+        <div
+          key={String(date.getMonth()) + String(i)}
+          className={styles.nextDate}
+        >
+          {i}
+        </div>
+      );
+    }
+
+    setDays(newDays);
+  }, [date]);
+
+  // get prev arrow
+  // prev.addEventListener("click", prevMonth);
+  // get next arrow
+  // next.addEventListener("click", nextMonth);
+
+  function nextMonth() {
+    // increase the month by 1
+    let newDate = new Date(date); // need to create new Date to trigger useEffect
+    newDate.setMonth(date.getMonth() + 1);
+    setDate(newDate);
+  }
+
+  function prevMonth() {
+    // decrease the month by 1
+    let newDate = new Date(date);
+    newDate.setMonth(date.getMonth() - 1);
+    setDate(newDate);
+  }
 
   return (
     <div
@@ -19,12 +137,16 @@ function Calendar() {
 
       <div className={styles.calendarFunction}>
         <div className={styles.month}>
-          <i className="material-icons prev">navigate_before</i>
+          <i className="material-icons prev" onClick={prevMonth}>
+            navigate_before
+          </i>
           <div className={styles.date}>
-            <h1></h1>
-            <p></p>
+            <h1>{months[date.getMonth()] + " " + date.getFullYear()}</h1>
+            <p>{new Date().toDateString()}</p>
           </div>
-          <i className="material-icons next">navigate_next</i>
+          <i className="material-icons next" onClick={nextMonth}>
+            navigate_next
+          </i>
         </div>
         <div className={styles.weekdays}>
           <div>Sun</div>
@@ -35,7 +157,7 @@ function Calendar() {
           <div>Fri</div>
           <div>Sat</div>
         </div>
-        <div className={styles.days}></div>
+        <div className={styles.days}>{days}</div>
       </div>
     </div>
   );
